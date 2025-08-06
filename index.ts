d62d4a9693da80c8f57ee86f5fec23f944492489
Charles <ultramixerman@gmail.com>
2025-08-05 20:29:19 -0500

    Fix mkdir

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
 import { existsSync } from 'node:fs';
 import { runFileServer } from './Server.js';
 import { mkdir } from 'node:fs/promises';
-import path from 'node:path';
 import { basePath } from './types.js';
 
 process.title = 'musicUploader';
 
 if (!existsSync(basePath)) {
   console.log('basePath missing creating...');
-  await mkdir(path.dirname(basePath), { recursive: true });
+  await mkdir(basePath, { recursive: true });
 }
 
 runFileServer.start();
