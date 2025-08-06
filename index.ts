21b382798dda39544f0067240a7239d8b1d4b730
Charles <ultramixerman@gmail.com>
2025-08-05 20:24:32 -0500

    Move file creation

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
-import { runFileServer } from './Server';
+import { existsSync } from 'node:fs';
+import { runFileServer } from './Server.js';
+import { mkdir } from 'node:fs/promises';
+import path from 'node:path';
+import { basePath } from './types.js';
+
 process.title = 'musicUploader';
+
+if (!existsSync(basePath)) {
+  await mkdir(path.dirname(basePath), { recursive: true });
+}
+
 runFileServer.start();
